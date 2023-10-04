7ab291955eaf8e7ee9dec70cbb5cb37609fd937f
Lamiaa21-3 <lamiaagamal213@gmail.com>
2023-10-04 10:58:41 +0200

    cubit

--- lib/cubits/weather_cubit/weather_state.dart
+++ lib/cubits/weather_cubit/weather_state.dart
@@ -1,6 +1,8 @@
+abstract class WeatherState {}
 
-abstract class WeatherState{}
+class LoadingWeatherState extends WeatherState {}
 
-class LoadingWeatherState extends WeatherState{}
-class SuccessWeatherState extends WeatherState{}
-class FailureWeatherState extends WeatherState{}
+class SuccessWeatherState extends WeatherState {}
+
+class FailureWeatherState extends WeatherState {}
+
